239bd54145da9a686799512fb4c24053b1bbbb6c
chenyangkang <chenyangkang24@outlook.com>
2024-03-25 19:23:16 +0800

    data download

---
+++ EnvArray/download/land_cover/request.py
@@ -0,0 +1,20 @@
+
+import earthaccess
+import my_secrets
+import os
+os.environ['EARTHDATA_USERNAME'] = my_secrets.username
+os.environ['EARTHDATA_PASSWORD'] =  my_secrets.password
+
+
+def MODIS_land_cover_download(start_date, end_date, save_path):
+    auth = earthaccess.login(strategy='environment')
+
+    results = earthaccess.search_data(
+        short_name='MCD12Q1', # here the modis product name
+        version='061', # for version 61
+        bounding_box = (-180,-90,180,90),
+        temporal = (start_date,end_date)
+    )
+
+    files = earthaccess.download(results, save_path)
+
--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Efficient daily environmental data processing and annotation with Xarray and net
 
 ## TODO
 
-### For EAR5 data
-- [] 1. Script for downloading data
-- [] 2. Loading data into Xarray
-- [] 3. Query example
-- [] 4. Resampling example (up and down)
+### For EAR5 data (https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-land?tab=overview)
+- [x] 1. Script for downloading data
+- [x] 2. Loading data into Xarray
+- [x] 3. Query example
+- [x] 4. Resampling example (up and down)
 
 ### For land cover data
 - [] 1. Script for downloading data
