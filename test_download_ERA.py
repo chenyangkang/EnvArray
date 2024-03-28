c13a2e0916b21ebd5c30a5a3d665d041d0dccff6
chenyangkang <chenyangkang24@outlook.com>
2024-03-28 19:07:02 +0800

    update

---
+++ test_download_ERA.py
@@ -0,0 +1,11 @@
+import os
+start_date='2000-01-01'
+end_date='2020-01-01'
+time_interval='1D'
+spatial_resolution='30km'
+output_folder='./downloaded_data'
+
+### For EAR5 data
+from EnvArray.download.ERA5.request import get_ERA5_data_and_process
+get_ERA5_data_and_process(start_date, end_date, time_interval, spatial_resolution, os.path.join(output_folder, 'ERA5'))
+
