e6570efe086b7cdb6d55b268e115170a805a8529
chenyangkang <chenyangkang24@outlook.com>
2024-03-29 10:24:56 +0800

    Mar29

--- test_download_ERA.py
+++ test_download_ERA.py
@@ -1,7 +1,7 @@
 import os
 
-start_date='2000-01-01'
-end_date='2020-01-01'
+start_date='2010-01-01'
+end_date='2024-01-01'
 time_interval='1D'
 spatial_resolution='30km'
 output_folder='./downloaded_data'
@@ -11,7 +11,9 @@ os.makedirs(output_folder, exist_ok=True)
 from EnvArray.download.ERA5.request import get_ERA5_data_and_process
 
 # Get and Process combined
-get_ERA5_data_and_process(start_date, end_date, time_interval, spatial_resolution, os.path.join(output_folder, 'ERA5'))
+ERA5_path = os.path.join(output_folder, 'ERA5')
+os.makedirs(ERA5_path, exist_ok=True)
+get_ERA5_data_and_process(start_date, end_date, time_interval, spatial_resolution, ERA5_path)
 
 # First get, then process
 # get_ERA5_data(start_date, end_date, time_interval, spatial_resolution, os.path.join(output_folder, 'ERA5'))
