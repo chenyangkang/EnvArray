f8b1cb4cdb1562b063affa3b898b6bc9c8a3f124
Yangkang <chenyangkang24@outlook.com>
2024-10-19 01:34:23 -0500

    Update daily_request_daily_processor.py

--- EnvArray/download/ERA5/daily_request_daily_processor.py
+++ EnvArray/download/ERA5/daily_request_daily_processor.py
@@ -78,34 +78,56 @@ class ERA5DataDailyRequestProcessor:
         except Exception as e:
             print(f"Error processing {year}-{month}-{day}: {e}")
 
+
     def get_data(self,year,month,day):
+        
+        dataset = "reanalysis-era5-single-levels"
+        request = {
+            "product_type": ["reanalysis"],
+            "variable": [
+                "10m_u_component_of_wind",
+                "10m_v_component_of_wind",
+                "2m_temperature",
+                "mean_sea_level_pressure",
+                "mean_wave_direction",
+                "sea_surface_temperature",
+                "surface_pressure",
+                "total_precipitation",
+                "100m_u_component_of_wind",
+                "100m_v_component_of_wind",
+                "surface_net_solar_radiation",
+                "surface_net_thermal_radiation",
+                "cloud_base_height",
+                "total_cloud_cover",
+                "lake_cover",
+                "lake_mix_layer_temperature",
+                "evaporation",
+                "snow_density",
+                "snow_depth",
+                "snowmelt",
+                "temperature_of_snow_layer",
+                "soil_temperature_level_1",
+                "high_vegetation_cover",
+                "leaf_area_index_high_vegetation",
+                "leaf_area_index_low_vegetation",
+                "low_vegetation_cover"
+            ],
+            
+            'year':year,
+            'month':month,
+            'day':day,
+            'time':['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00',
+                    '09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00',
+                    '17:00','18:00','19:00','20:00','21:00','22:00','23:00'],
+            "data_format": "netcdf",
+            "download_format": "unarchived"
+        }
+
         c=cdsapi.Client()
-        c.retrieve(
-            'reanalysis-era5-single-levels',
-            {   
-                'product_type': 'reanalysis',
-                'format': 'netcdf',
-                'variable':[
-                    '10m_u_component_of_wind', '10m_v_component_of_wind', '100m_u_component_of_wind','100m_v_component_of_wind',
-                    '2m_temperature','2m_dewpoint_temperature','sea_surface_temperature',
-                    'surface_pressure', 'mean_sea_level_pressure',
-                    'total_cloud_cover', 'low_cloud_cover','medium_cloud_cover','high_cloud_cover','cloud_base_height',
-                    'total_precipitation','large_scale_precipitation',
-                    'lake_cover', 'lake_mix_layer_temperature',
-                    'snowmelt','snow_depth',
-                    'surface_latent_heat_flux', 'surface_net_solar_radiation','downward_uv_radiation_at_the_surface',
-                    'high_vegetation_cover','low_vegetation_cover','leaf_area_index_high_vegetation','leaf_area_index_low_vegetation',
-                    'significant_height_of_combined_wind_waves_and_swell','mean_wave_direction'
-                ],
-                'year':year,
-                'month':month,
-                'day':day,
-                'time':['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00',
-                        '09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00',
-                        '17:00','18:00','19:00','20:00','21:00','22:00','23:00'],
-            },
+        c.retrieve(dataset, request,
             os.path.join(self.output_folder, f'download_ERA5_{year}_{month}_{day}.nc')
         )
+                
 
     def process_ERA5(self, year, month, day):
         data = xr.open_dataset(os.path.join(self.output_folder, f'download_ERA5_{year}_{month}_{day}.nc'))
